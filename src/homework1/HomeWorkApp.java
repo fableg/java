0daad73c033a9384a0285996560d15c21659df1d
fableg <Fhneh9989>
2021-04-16 21:33:55 +0300

    else if removed

--- src/homework1/HomeWorkApp.java
+++ src/homework1/HomeWorkApp.java
@@ -24,14 +24,12 @@ public class HomeWorkApp {
     }
 
     public static void printColor() {
-        int value = 554;
+        int value = 0;
         if (value <= 0) {
             System.out.println("Красный");
         } else if (value <= 100){
             System.out.println(" Жёлтый");
-        } else if (value > 100){
-            System.out.println("Зеленый");
-        }
+        } else System.out.println("Зеленый");
     }
 
     public static void compareNumbers() {
