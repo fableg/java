01cf6f979f81220bca26562b780032af439440e3
Arthur_Abuzyarov <fable777@gmail.com>
2021-04-20 12:57:42 +0300

    add int summ in 1 task

--- src/homework2/HomeWorkApp2.java
+++ src/homework2/HomeWorkApp2.java
@@ -2,18 +2,15 @@ package homework2;
 
 public class HomeWorkApp2 {
     public static void main(String[] args) {
-    within10and20();
-    isNegativeOrPositive();
-    isNegative();
-    printWordNTimes();
-    checkLeapYear();
+        within10and20(50,5);
     }
     // Написать метод, принимающий на вход два целых числа и проверяющий,
 // что их сумма лежит в пределах от 10 до 20 (включительно),
 // если да – вернуть true, в противном случае – false.
 
     public static boolean within10and20 (int a, int b){
-        if (a + b >= 10 && a + b <= 20) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
             return true;
         } else {
             return false;
