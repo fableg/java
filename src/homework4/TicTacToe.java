42d6a771db5638e1a745269dca11bfce2bb5fbe8
Arthur_Abuzyarov <fable777@gmail.com>
2021-04-27 16:42:54 +0300

    code from lesson without homework

--- src/homework4/TicTacToe.java
+++ src/homework4/TicTacToe.java
@@ -1,6 +1,7 @@
 package homework4;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -17,8 +18,57 @@ public class TicTacToe {
     public static void main(String[] args) {
         initMap();
         printMap();
-        humanTurn();
 
+        while (true) {
+            humanTurn();
+            printMap();
+            if (isWin(DOT_X)) {
+                System.out.println("Human win");
+                break;
+            }
+            if (isMapFull()){
+                System.out.println("Ничья");
+                break;
+            }
+            computerTurn();
+            printMap();
+            if (isWin(DOT_O)) {
+                System.out.println("Machine win");
+                break;
+            }
+            if (isMapFull()){
+                System.out.println("Ничья");
+                break;
+            }
+        }
+
+    }
+
+    private static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // дз
+    private static boolean isWin(char symbol) {
+        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
+        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
+        if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
+
+        if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
+        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
+        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
+
+        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
+
+        return false;
     }
 
     private static void printMap() {
@@ -27,26 +77,32 @@ public class TicTacToe {
         System.out.println();
     }
 
+    private static void computerTurn() {
+        int x;
+        int y;
+
+        Random random = new Random();
+        do {
+       x = random.nextInt(SIZE);
+       y = random.nextInt(SIZE);
+        } while (map[x][y] != DOT_EMPTY);
+
+        map[x][y] = DOT_O;
+    }
+
     private static void humanTurn() {
-        int x = -1;
-        int y = -1;
+        int x;
+        int y;
         Scanner scanner = new Scanner(System.in);
         do {
             System.out.println("Введите координаты в формате X Y");
-            if (scanner.hasNextInt()) {
-                x = scanner.nextInt() - 1;
-            } else {
-                System.out.println("Координаты должны быть числом!");
-                scanner.nextLine();
-            }
-            if (scanner.hasNextInt()) {
-                y = scanner.nextInt() - 1;
-            } else {
+            x = readInt(scanner) - 1;
+            y = readInt(scanner) - 1;
+
+            if (x == -1 || y == -1) {
                 System.out.println("Координаты должны быть числом");
                 scanner.nextLine();
-            }
-
-            if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+            } else if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
                 System.out.println("Данные введены некорректно!");
             }
             else if (map[x][y] != DOT_EMPTY) {
@@ -58,7 +114,10 @@ public class TicTacToe {
             map[x][y] = DOT_X;
     }
 
-    private static
+    private static int readInt(Scanner scanner) {
+        return scanner.hasNextInt() ? scanner.nextInt() : -1;
+
+    }
 
     private static void printBody() {
         for (int i = 0; i < SIZE; i++) {
