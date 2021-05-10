b9e083865b938861996446b6d81f54651d48f2a0
Arthur_Abuzyarov <fable777@gmail.com>
2021-05-10 13:32:08 +0300

    homework5 complete

---
+++ src/homework7/Test.java
@@ -0,0 +1,11 @@
+package homework7;
+
+public class Test {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 5);
+        Plate plate = new Plate(100);
+        plate.info();
+        cat.eat(plate);
+        plate.info();
+    }
+}
